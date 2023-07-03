b1c6cc6024394f7e245791849c4ff24f93ca1d8d
Sarthak Mittal <sarmittal@gmail.com>
2023-07-03 22:27:44 +0530

    add

--- javascript/2.3+NPM/2.3 NPM/index.js
+++ javascript/2.3+NPM/2.3 NPM/index.js
@@ -1,3 +1,7 @@
-var generateName = require('sillyname');
-var sillyName = generateName();
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log(`my name is ${sillyName}`);
+
+import sn from "sillyname";
+var sillyName = sn();
 console.log(`my name is ${sillyName}`);
