02ae7c67caeb39abebc42c126ec550646c48ba34
Sarthak Mittal <sarmittal@gmail.com>
2023-07-03 22:23:08 +0530

    add

--- javascript/2.2+Native+Modules/2.2 Native Modules/index.js
+++ javascript/2.2+Native+Modules/2.2 Native Modules/index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
+fs.readFile("./hello.txt","utf8",(err,data)=> {
+    if (err) throw err;
+    console.log(data);
+})
 
 // fs.writeFile("hello.txt", "hello my name is sarthak", (err)=>{
 // if(err) throw err;
