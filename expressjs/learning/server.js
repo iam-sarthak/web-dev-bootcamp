3c19a87b6ac5a51511bf8bb4b1da02d17e207139
Sarthak Mittal <sarmittal@gmail.com>
2023-07-04 22:04:28 +0530

    change

--- expressjs/learning/server.js
+++ expressjs/learning/server.js
@@ -9,6 +9,10 @@ app.get("/", function (req, res) {
     res.send("<h1>Hello broo!!!!!!!</h1>")   
 })
 
+app.get("/contact", function (req, res) {
+    res.send("<h1>Hello broo!!!!!!! I am Contact Page.</h1>")   
+})
+
 app.listen(3000, function () {
     console.log("server started");
 });
