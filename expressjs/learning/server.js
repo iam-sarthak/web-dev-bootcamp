ea00001c6eecadf67ce07e8b33f0ca37c70a005f
Sarthak Mittal <sarmittal@gmail.com>
2023-07-04 20:51:38 +0530

    express

---
+++ expressjs/learning/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+// app.get("/", function (req, res) {
+//     console.log(req);    
+// })
+app.get("/", function (req, res) {
+    res.send("<h1>Hello broo!!!!!!!</h1>")   
+})
+
+app.listen(3000, function () {
+    console.log("server started");
+});
